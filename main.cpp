58c824a4470ba850d79394457ffc4fd0da4b01ad
Aula <Aula@EQUIP079574.xunta.local>
2020-10-26 09:53:29 +0100

    Crear función.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+#define N 10
+
+void cuenta_atras(string nombre) {
+    for (int contador = N; contador >= 0; contador--)
+    {
+        cout << "Cuenta atrás " << nombre << ": " << contador  << endl;
+    }
+    
+}
+
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
 }
