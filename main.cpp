76bee0dad5654e55a089c51416b5494468564aab
Aula <Aula@EQUIP079574.xunta.local>
2020-10-26 09:57:19 +0100

    Cuenta atrás sencilla.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <string>
-using namespace std;
+#include <thread>
+#include <chrono>
 
 #define N 10
 
-void cuenta_atras(string nombre) {
+void cuenta_atras(std::string nombre) {
     for (int contador = N; contador >= 0; contador--)
     {
-        cout << "Cuenta atrás " << nombre << ": " << contador  << endl;
+        std::cout << "Cuenta atras " << nombre << ": " << contador  << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
     
 }
 
 
 int main(int, char**) {
+    cuenta_atras("prueba");
 }
